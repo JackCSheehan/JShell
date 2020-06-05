c50f4716f728979bf504c92ace2c998a935c44cb
jackcsheehan <jackcsheehan@users.noreply.github.com>
2020-06-04 22:45:53 -0500

    Added unknown commands error

--- src/errors.asm
+++ src/errors.asm
@@ -1,7 +1,23 @@
 ; Labels that show error messages
 
-; Error that shows when there are not enough args given to a command
+; Error that shows when there are not enough args given to a command. Preserves EAX.
 showTooFewArgsErr:
+	push	eax	
+
 	mov	eax, tooFewArgsErr
 	call	println
+	
+	pop	eax
+
+	jmp	repeat
+
+; Error that shows when there are not enough args given to a command. Preserves EAX.
+showNoCommErr:
+	push	eax
+
+	mov	eax, noCommErr
+	call	println
+	
+	pop	eax
+
 	jmp	repeat
